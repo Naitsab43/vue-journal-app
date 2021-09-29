f8c7945c686e2373cc3252b539c897e00331291a
Naitsab43 <bastianhuenuqueo43@gmail.com>
2021-09-29 00:02:54 -0300

    Subir a github

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div @click="$router.push({name: 'entry', params: {id: 1} })" class="entry-container mb-3 pointer p-2">
+
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">15</span>
+      <span class="mx-1 fs-5">Septiembre</span>
+      <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+
+    <div class="entry-description">
+      Nostrud occaecat incididunt anim nostrud ea aliquip consectetur sit do qui eiusmod anim. Adipisicing aute et irure laborum quis labore cupidatat ea excepteur non sint.
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+  name: "Entry",
+  data(){
+    return {}
+  },
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+}
+
+.entry-description {
+  font-size: 12px;
+}
+
+</style>
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,71 @@
+<template>
+  
+  <div class="entry-title d-flex justify-content-between p-2">
+
+    <span class="text-success fs-3 fw-bold">15</span>
+    <span class="mx-1 fs-3">Septiembre</span>
+    <span class="mx-2 fs-3 fw-light">2021, jueves</span>
+
+  </div>
+
+  <div>
+
+    <button class="btn btn-danger mex-2">
+      Borrar
+      <i class="fa fa-trash-alt"></i>
+    </button>
+
+    <button class="btn btn-primary">
+      Subir imagen
+      <i class="fa fa-upload"></i>
+    </button>
+
+  </div>
+
+  <hr>
+
+  <div class="d-flex flex-column px-3 h-75">
+
+    <textarea placeholder="¿Que sucedio hoy?"></textarea>
+
+  </div>
+
+  <Fab 
+    icon="fa-save"
+  />
+
+  <img class="img-thumbnail" src="https://conceptodefinicion.de/wp-content/uploads/2019/06/letras-1.jpg" alt="A">
+
+</template>
+
+<script>
+import { defineAsyncComponent } from '@vue/runtime-core'
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import("../components/Fab.vue"))
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+textarea {
+  font-size: 20px;
+  border: none;
+  height: 100%;
+
+  &:focus {
+    outline: none;
+  }
+
+}
+
+img {
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+
+</style>
